5d5342b838a4d2b9cfe7b7f0fbcfa792dc7ad85e
theam-dev <phearakroun@gmail.com>
2023-04-02 15:58:53 +0700

    Remove Row

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Tutorial Sharing</a>
+    </div>
+  </nav>
+  <div class="container mt-3">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.email }}</td>
+          <td>
+            <button class="btn btn-primary btn-sm" v-on:click="removeRow(index)">remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      data: [
+        {"id": 1,"name": "Francis","email": "Marley.Hermiston64@hotmail.com"},
+        {"id": 2,"name": "Jay","email": "Amanda.White56@gmail.com"},
+        {"id": 3,"name": "Marianna","email": "Kiarra.DuBuque@gmail.com"},
+        {"id": 4,"name": "Kamron","email": "Patience13@gmail.com"},
+        {"id": 5,"name": "Antone","email": "Armando.Bauch@gmail.com"}
+      ]
+    }
+  },
+  methods:{
+    removeRow(index){
+      this.data.splice(index, 1);
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
